d4b789ae256eae2e4b8194868022dd864e9eaf4f
fallencloud <sharinavjones@gmail.com>
2019-05-09 18:01:06 -0400

    Updates

---
+++ precache-manifest.5664bb454c0c2017ef11a8e6f3763748.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "591a2a132b29cf04c2c7",
+    "url": "/flash_card_react/static/js/runtime~main.d70c2786.js"
+  },
+  {
+    "revision": "96c89d69d29ace902ad4",
+    "url": "/flash_card_react/static/js/main.bd1d3d42.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/flash_card_react/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a341d13e9562bb266630d22f12de4c53",
+    "url": "/flash_card_react/index.html"
+  }
+];
